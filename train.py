9ba692328438e3e435c0808b17f248c9e2ed7b91
Vince-C156 <82852515+Vince-C156@users.noreply.github.com>
2022-08-04 19:50:56 -0700

    Add files via upload

--- train.py
+++ train.py
@@ -7,17 +7,17 @@ from tensorflow.keras.callbacks import TensorBoard
 
 
 
-model_dir = "model_out/ARPOD_AGENT"
-log_dir = "model_out/ARPOD_AGENT/logs"
+model_dir = "model_out/ARPOD_AGENT3"
+log_dir = "model_out/ARPOD_AGENT3/logs"
 tensorboard = TensorBoard(log_dir=log_dir)
-x0 = [500.0, -500.0, 500.0, 0.0, 0.0, 0.0] 
+x0 = [200.0, 100.0, 250.0, 0.0, 0.0, 0.0] 
 env = HCW_ARPOD(x0) 
 env.reset()
  
 model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=log_dir)
 
 for i in range(1000):
-    model.learn(total_timesteps=1000, reset_num_timesteps=False, tb_log_name="ARPOD_AGENT")
+    model.learn(total_timesteps=500, reset_num_timesteps=False, tb_log_name="ARPOD_AGENT3")
     model.save(model_dir)
 
 env.close()
